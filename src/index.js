05926f3323abb2407aa974c7e494f9d71d4cdd07
kos4 <kos4.kks@gmail.com>
2024-07-30 15:04:47 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './css/style.css';
 import './js/app';
+
+if (navigator.serviceWorker) {
+  window.addEventListener('load', async () => {
+    try {
+      if (navigator.serviceWorker) {
+        await navigator.serviceWorker.register(
+          './service-worker.js',
+        );
+        console.log('sw registered');
+      }
+      // await registration.unregister();
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
